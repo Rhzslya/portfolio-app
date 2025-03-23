ab530b6a240543c40a83236dffc4d967c1ba7723
Rhzslya <rhzslyaaa@gmail.com>
2025-03-23 16:40:17 +0700

    responsive contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,16 +3,14 @@ import { motion } from "framer-motion";
 import { fadeIn } from "@/utils/FramerMotionStyle";
 import { LocationIcon, MailIcon, PhoneIcon } from "@/utils/Icon";
 import { BackgroundGradient } from "../ui/background-gradient";
-import dynamic from "next/dynamic";
+import Map from "../LeafletMaps";
 
-// Import Map dengan SSR dinonaktifkan
-const Map = dynamic(() => import("@/components/LeafletMaps"), { ssr: false });
 const Contact = () => {
   return (
-    <section className="contact-root">
+    <section className="contact-root mx-auto max-w-7xl">
       <div className="contact-container">
         <motion.h1
-          className="text-gray-300 font-bold text-center relative z-10 text-nowrap"
+          className="text-gray-300 font-bold text-center relative z-10 text-nowrap mb-8"
           style={{ fontSize: "clamp(2.5rem, 5vw, 5rem)" }}
           initial="hidden"
           animate="visible"
@@ -21,57 +19,58 @@ const Contact = () => {
           Get In Touch
         </motion.h1>
 
-        <div className="contact-content p-2 grid grid-cols-3 gap-4">
-          <div className="w-full flex flex-col items-center gap-4">
-            <BackgroundGradient className="">
-              <LocationIcon
-                color="#fff"
-                size={60}
-                className="background-main p-2 rounded-full"
-              />
-            </BackgroundGradient>
+        <div className="contact-content grid grid-cols-auto lg:grid-cols-2 grid-rows-1 gap-4">
+          <div className="flex flex-col gap-8 lg:gap-16 py-2 px-0 sm:px-4 my-auto">
+            <div className="w-full flex flex-col lg:flex-row items-center gap-4 lg:gap-6">
+              <BackgroundGradient>
+                <PhoneIcon
+                  color="#fff"
+                  size={60}
+                  className="background-main p-2 rounded-full"
+                />
+              </BackgroundGradient>
 
-            <div className="address max-w-[70%]">
-              <p className="text-center text-base font-medium">
-                Jl. Guru Kojar, Kampung Pondok Belimbing, Kelurahan Jurang Mangu
-                Barat, Kecamatan Pondok Aren, Kota Tangerang Selatan, Banten
-                15224
-              </p>
-            </div>
-            {/* <Map /> */}
-          </div>
-          <div className=" w-full flex flex-col items-center gap-4">
-            <BackgroundGradient>
-              <PhoneIcon
-                color="#fff"
-                size={60}
-                className="background-main p-2 rounded-full"
-              />
-            </BackgroundGradient>
-
-            <div className="w-full h-full p-2 rounded-md flex flex-col justify-center items-center">
-              <div className="bg-amber-500 text-white px-2 py-1 rounded-md">
-                <p className="text-base mt-1">Available 24/7</p>
+              <div className="phone">
+                <p className="text-center lg:text-left text-sm sm:text-base font-semibold">
+                  +62 851 8339 2058
+                </p>
               </div>
-              <p className="text-xl font-bold text-center text-nowrap text-amber-500">
-                +62 851 8339 2058
-              </p>
+            </div>
+
+            <div className="w-full flex flex-col lg:flex-row  items-center gap-4 lg:gap-6">
+              <BackgroundGradient>
+                <MailIcon
+                  color="#fff"
+                  size={60}
+                  className="background-main p-2 rounded-full"
+                />
+              </BackgroundGradient>
+              <div className="email">
+                <p className="text-center lg:text-left text-sm sm:text-base font-semibold ">
+                  rizqisabilla1@gmail.com
+                </p>
+              </div>
+            </div>
+            <div className="w-full flex flex-col lg:flex-row  items-center gap-4 lg:gap-6">
+              <BackgroundGradient className="">
+                <LocationIcon
+                  color="#fff"
+                  size={60}
+                  className="background-main p-2 rounded-full"
+                />
+              </BackgroundGradient>
+
+              <div className="address w-full max-w-[90%] sm:max-w-[80%] md:max-w-[60%] lg:max-w-[100%] ">
+                <p className="text-center lg:text-left text-sm sm:text-base font-semibold">
+                  Jl. Guru Kojar, Kampung Pondok Belimbing, Kelurahan Jurang
+                  Mangu Barat, Kecamatan Pondok Aren, Kota Tangerang Selatan,
+                  Banten 15224
+                </p>
+              </div>
             </div>
           </div>
-          <div className="w-full flex flex-col items-center gap-2">
-            <BackgroundGradient>
-              <MailIcon
-                color="#fff"
-                size={60}
-                className="background-main p-2 rounded-full"
-              />
-            </BackgroundGradient>
-            <div className="address max-w-[70%]">
-              <p className="text-center">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Officiis, vero!
-              </p>
-            </div>
+          <div className="flex justify-center items-center rounded-md min-h-[300px] sm:min-h-[350px] md:min-h-[450px] lg:min-h-[350px] p-0 sm:p-4">
+            <Map />
           </div>
         </div>
       </div>
--- src/components/LeafletMaps/index.tsx
+++ src/components/LeafletMaps/index.tsx
@@ -1,39 +1,7 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-
-// Konfigurasi custom icon
-const customIcon = new L.Icon({
-  iconUrl: "/assets/leaflet/icons/marker.svg", // Arahkan ke file di `public/`
-  iconSize: [30, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+const Map = dynamic(() => import("./Map"), {
+  ssr: false,
 });
-
-const Map = () => {
-  return (
-    <MapContainer
-      center={[-6.25652, 106.72318]}
-      zoom={13}
-      style={{
-        height: "350px",
-        width: "100%",
-        borderRadius: "15px",
-        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
-        overflow: "hidden",
-        zIndex: "10",
-      }}
-      dragging={false}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[-6.25652, 106.72318]} icon={customIcon}>
-        <Popup>Ini Lokasi Saya!</Popup>
-      </Marker>
-    </MapContainer>
-  );
-};
 
 export default Map;
