f63631588fccb720e9b5ff4c0484a51c0de04da1
Rhzslya <rhzslyaaa@gmail.com>
2025-04-02 18:45:51 +0700

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import React from "react";
 import { FlipWords } from "../ui/flip-words";
+import { sectionsNav } from "@/utils/Sections";
+import { capitalizeFirst } from "@/utils/Capitalize";
 
 const Footer = () => {
   const words = [
@@ -12,6 +14,21 @@ const Footer = () => {
     "Innovate & Elevate",
     "Code Beyond Limits",
     "Dream. Code. Achieve.",
+  ];
+
+  const socialMedia = [
+    {
+      name: "facebook",
+      url: "https://www.facebook.com/profile.php?id=100077692014108&locale=id_ID",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/your_username",
+    },
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/your_username",
+    },
   ];
 
   return (
@@ -28,6 +45,46 @@ const Footer = () => {
           </Link>
           <div className="flex justify-center items-center mt-2">
             <FlipWords words={words} />
+          </div>
+          <div className="right-content">
+            <div className="flex flex-col gap-4 p-6">
+              <h3 className="text-white font-semibold">Quick Links</h3>
+              {sectionsNav?.map((item, index) => {
+                const linkPath = `/${item}`;
+                return (
+                  <Link
+                    href={linkPath}
+                    key={index}
+                    className="relative group hover:text-white duration-300 text-base font-medium"
+                  >
+                    {capitalizeFirst(item)}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="border-t-[1px] border-gray-500">
+          <div className="social_media_box hidden mt-8 sm:flex items-center justify-center gap-x-4 text-white text-[28px] mx-8">
+            {socialMedia.map((social, index) => (
+              <Link
+                key={index}
+                href={social.url}
+                className="relative group hover:text-gray-400 duration-300"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i
+                  className={`fi fi-brands-${social.name} flex items-center justify-center`}
+                ></i>
+              </Link>
+            ))}
+          </div>
+          <div className="copy-right-mark text-center mt-6 text-gray-400 text-sm">
+            <p>
+              © {new Date().getFullYear()} Rizqi Sabilla. Built with ❤️ &
+              Next.js.
+            </p>
           </div>
         </div>
       </div>
